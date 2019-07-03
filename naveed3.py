9e008d0560a364943dd55e848e61b941053606c6
shaik naveed ahamed <44160062+naveed7860@users.noreply.github.com>
2019-07-03 10:54:42 +0530

    Update naveed3.py

--- naveed3.py
+++ naveed3.py
@@ -1,8 +1,9 @@
-number=int(input()
-	if (number%2!=0):
+number=int(input())
+if (number%2!=0):
 	print("Odd")
-	elseif(number%2==0):
-	print("Even")
-	else:
-	print("Invalid")
-	
+elif(number%2==0):
+  print("Even")
+else:
+   print("Invalid")
+  
+
